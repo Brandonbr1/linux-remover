63405177975cad313bed2bbb268bef2536788b81
Java sauce <mohammedbrandon204@gmail.com>
2022-09-18 13:19:14 -0700

    Update and rename russianrouletlinux.bat to russianrouletlinux.sh
    changed file extension and added if statement

--- russianrouletlinux.bat
+++
@@ -1,5 +0,0 @@
-NUM=$(shuf -i 1-8 -n 1)
-
-r=$(( $RANDOM % 10 )); echo $r
-
-echo $NUM
---
+++ russianrouletlinux.sh
@@ -0,0 +1,11 @@
+NUM=$(shuf -i 1-8 -n 1)
+
+r=$(( $RANDOM % 10 )); echo $r
+
+if [ $RANDOM -eq $1 ]
+then
+        echo "Test"
+else
+        echo "Your Linux machine is safe"
+
+echo $NUM

